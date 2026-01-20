cf9ec2240e360b9798039859a8ed5075e79541ed
Arosh59 <aroshdanza59@gmail.com>
2026-01-20 09:06:12 +0530

    backend and frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoute from "./routes/auth.route.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoute);
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on PORT:" + PORT);
--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -2,19 +2,19 @@ import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 
 
-export const protectRoute = async (userId, res) => {
+export const protectRoute = async (req, res, next) => {
     try{
-        const token = req.cookies.jwt
+        const token = req.cookies.jwt;
 
         if(!token){
             return res.status(401).json({ message: "Unauthorized - No Token Provided" });
 
         }
 
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
         if(!decoded){
-           return res.status(401).json({ message: "Unauthorized -IN valid Token" }); 
+           return res.status(401).json({ message: "Unauthorized - Invalid Token" }); 
         }
 
         const user = await User.findById(decoded.userId).select("-password");
@@ -23,13 +23,19 @@ export const protectRoute = async (userId, res) => {
             return res.status(404).json({ message: "User not found" });
         }
 
-        req.user = user
+        req.user = user;
 
-        next()
+        next();
 
 
 
     } catch (error) {
+        if (error.name === "TokenExpiredError") {
+        return res.status(401).json({ message: "Unauthorized - Token Expired" });
+    }
+    if (error.name === "JsonWebTokenError") {
+        return res.status(401).json({ message: "Unauthorized - Invalid Token" });
+    }
         console.log("Error in protectRoute middleware: ", error.message);
         res.status(500).json({ message: "Internal server error" });
 
